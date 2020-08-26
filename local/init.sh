aa08660c1ee77ccdd798fccb6e91d282e339631d
B. Seeger <bseeger@users.noreply.github.com>
2020-08-26 17:40:33 -0400

    Updates based on upstream changes. (#1)

--- local/init.sh
+++ local/init.sh
@@ -15,6 +15,7 @@ function checkout { # checkout $repo $branch $into_dir
   fi
 }
 
+echo "[ISLE DC] Checking out ISLE"
 checkout $ISLE_REPO $ISLE_BRANCH .isle
 checkout $DRUPAL_SITE_REPO $DRUPAL_SITE_BRANCH codebase
 
@@ -23,8 +24,14 @@ make
 if [[ $(docker-compose ps -q) ]] ; then
 	docker-compose down -v
 fi
+
+echo "[ISLE DC] Starting ISLE..."
 docker-compose up -d
+echo "[ISLE DC] Composer install..."
 docker-compose exec drupal with-contenv bash -lc 'COMPOSER_MEMORY_LIMIT=-1 composer install'
+echo "[ISLE DC] make install..."
 make install
+echo "[ISLE DC] updating/managing settings..."
 make update-settings-php update-config-from-environment solr-cores run-islandora-migrations
+echo "[ISLE DC] rebuilding Drupal cache..."
 docker-compose exec drupal drush cr -y
