dfbef431609429968803bb6b4a4e34ef710d3a84
Aaron Birkland <apb@jhu.edu>
2020-10-06 11:16:24 -0400

    whoops, forgot to remove s3fs_cors vendored files

--- local/bootstrap.sh
+++ local/bootstrap.sh
@@ -1,8 +1,15 @@
+echo "Destroying old ISLE state"
+docker-compose down -v
+echo "building empty snapshot image"
+docker-compose build snapshot
 echo "[ISLE DC] Starting ISLE..."
 docker-compose up -d
 echo "[ISLE DC] make install..."
+sleep 5
 make install
 echo "[ISLE DC] updating/managing settings..."
 make update-settings-php update-config-from-environment solr-cores run-islandora-migrations
 echo "[ISLE DC] rebuilding Drupal cache..."
 docker-compose exec drupal drush cr -y
+echo "Taking snapshot of new install"
+make snapshot-image
